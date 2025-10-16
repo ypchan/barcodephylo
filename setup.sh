d91ae7a8c12388e2ea18562e4c55f39b025abfde
Yanpeng Chen <chenyanpeng1992@outlook.com>
2025-10-16 16:55:46 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,3 +5,8 @@ ls scripts | grep '.py' | while read s; do
     ln -s $(pwd)/scripts/${s} ~/bin/${s}
     echo "ln -s $(pwd)/scripts/${s} ~/bin/${s}"
 done
+
+chmod 755 barcodephylo.sh
+echo "chmod 755 barcodephylo.sh"
+ln -s $(pwd)/barcodephylo.sh ~/bin/barcodephylo.sh
+echo "ln -s $(pwd)/barcodephylo.sh ~/bin/barcodephylo.sh"
